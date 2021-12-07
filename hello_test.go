dc85adaee1accba8a7f25018e94a6e378bc96c30
nergal-perm <nergal@psu.ru>
2021-12-07 09:17:26 +0500

    Personalized greeting

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectGreeting := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
 
+	t.Run("Given a name, should greet personally", func(t *testing.T) {
+		got := Hello("Eugene")
+		want := "Hello, Eugene!"
+		assertCorrectGreeting(t, got, want)
+	})
+
+	t.Run("Given a blank name, should show default greeting", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCorrectGreeting(t, got, want)
+	})
 }
