69a30ec15d91b2fcf5dd15eb993624c79b29485a
nergal-perm <nergal@psu.ru>
2021-12-10 10:13:27 +0500

    slice tail sum function

--- 04-arrays/arrays_test.go
+++ 04-arrays/arrays_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -15,5 +16,22 @@ func TestSum(t *testing.T) {
 	t.Run("should sum numbers from 1 to 10", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 		assertCorrectSum(t, Sum(numbers), 55)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertCorrectTails := func(t testing.TB, actual, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expected '%v' but got '%v", expected, actual)
+		}
+	}
+
+	t.Run("should sum all tails", func(t *testing.T) {
+		assertCorrectTails(t, SumAllTails([]int{1, 2}, []int{0, 9}), []int{2, 9})
+	})
+
+	t.Run("should handle empty slices safely", func(t *testing.T) {
+		assertCorrectTails(t, SumAllTails([]int{}, []int{0, 9}), []int{0, 9})
 	})
 }
